bf3b20e10b4d4c22b74512b721cd3824bb890ffd
RobeSantoro <santoro.robe@gmail.com>
2022-08-24 04:20:35 +0200

    Refactor, newGame function

--- main.js
+++ main.js
@@ -1,34 +1,34 @@
 import './style.css'
 
-// PREP ARRAYs
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
-const colors = ['red', 'blue', 'green', 'cyan', 'yellow', 'magenta', 'black', 'orange']
-
-// Duplicate and shuffle the color array
-colors.push(...colors)
-colors.sort(() => {
-  return Math.random() - 0.5;
-})
-
-const cards = []
-let numFlipped = 0
-let flipped = []
-let moves = 0
-
 // DOM
 const app = document.getElementById('app')
+const h1 = document.createElement('h1')
 const board = document.createElement('div')
 
-app.innerHTML = '<h1>Memory</h1>'
-app.appendChild(board)
 board.className = 'board'
 
-// Create the board and the cards array
-numbers.forEach(number => {
+app.appendChild(h1)
+app.appendChild(board)
 
-})
+function newGame() {
+  
+  h1.innerHTML = 'Memory'
 
-function generateBoard() {
+  let numFlipped = 0
+  let flipped = []
+  let moves = 0
+
+  // PREP ARRAYs
+  const cards = []
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+  const colors = ['red', 'blue', 'green', 'cyan', 'yellow', 'magenta', 'black', 'orange']
+
+  // Duplicate and shuffle the color array
+  colors.push(...colors)
+  colors.sort(() => {
+    return Math.random() - 0.5;
+  })
+
   // Card Creation
   for (let i = 0; i < numbers.length; i++) {
 
@@ -40,81 +40,84 @@ function generateBoard() {
     element.className = 'card'
     element.id = card.number
     element.innerHTML = card.number
+    board.style.display = 'grid'
     board.appendChild(element)
 
     cards.push(card)
   }
-}
 
-generateBoard()
+  // Cycle through created cards and game logic
+  cards.forEach(card => {
 
-// Cycle through cards and game logic
-cards.forEach(card => {
+    card.element.addEventListener('mousedown', () => {
 
-  card.element.addEventListener('mousedown', () => {
+      if (numFlipped < 2 && card.flipped == false) {
 
-    if (numFlipped < 2 && card.flipped == false) {
+        numFlipped++
+        flipped.push(card)
 
-      numFlipped++
-      flipped.push(card)
+        card.flipped = true
+        card.element.style.backgroundColor = card.color
 
-      card.flipped = true
-      card.element.style.backgroundColor = card.color
+        // MATCH
+        if (flipped.length == 2 && flipped[0].color == flipped[1].color) {
 
-      // MATCH
-      if (flipped.length == 2 && flipped[0].color == flipped[1].color) {
+          let index = cards.map(obj => obj.color).indexOf(flipped[0].color)
+          cards.splice(index, 1) // SKETCHY
+          index = cards.map(obj => obj.color).indexOf(flipped[0].color)
+          cards.splice(index, 1) // SKETCHY
 
-        let index = cards.map(obj => obj.color).indexOf(flipped[0].color)
-        cards.splice(index, 1) // SKETCHY
-        index = cards.map(obj => obj.color).indexOf(flipped[0].color)
-        cards.splice(index, 1) // SKETCHY
-
-        console.table(cards);
-
-        flipped = []
-        numFlipped = 0
-
-        // WIN
-        if (cards.length == 14) {
-          board.style.display = 'none'
-          app.innerHTML = '<h1>YOU WIN!</h1>'
-
-          const playButton = document.createElement('button')
-          playButton.innerText = 'Play Again'
-          playButton.classList.add('button')
-          app.appendChild(playButton)
-
-          playButton.addEventListener('click', () => {
-            
-            generateBoard();
-            playButton.style.display = 'none'
-          })
-
-          console.log('YOU WIN')
-        }
-      }
-
-      // NO MATCH
-      else if (flipped.length == 2) {
-
-        setTimeout(() => {
-          cards.forEach(card => {
-            if (card.flipped == true) {
-              card.flipped = false
-              card.element.style = 'none'
-            }
-          })
+          /* console.table(cards); */
 
           flipped = []
           numFlipped = 0
 
-        }, 1000)
-      }
-    }
-  })
-})
+          // WIN
+          if (cards.length == 0) {
 
-console.table(cards);
+            board.style.display = 'none'
+            h1.innerHTML = 'YOU WIN!'
+
+            const playButton = document.createElement('button')
+            playButton.innerText = 'Play Again'
+            playButton.classList.add('button')
+            app.appendChild(playButton)
+
+            playButton.addEventListener('click', () => {
+              board.innerHTML = ''
+              newGame();
+              app.removeChild(playButton)
+            })
+          }
+        }
+
+        // NO MATCH
+        else if (flipped.length == 2) {
+
+          setTimeout(() => {
+            cards.forEach(card => {
+              if (card.flipped == true) {
+                card.flipped = false
+                card.element.style = 'none'
+              }
+            })
+
+            flipped = []
+            numFlipped = 0
+
+          }, 1000)
+        }
+      }
+    })
+  })
+
+  console.table(cards);
+  return cards
+}
+
+newGame()
+
+
 
 
 
