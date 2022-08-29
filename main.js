c450acade897f4d36206678a642ffef40b87f750
RobeSantoro <santoro.robe@gmail.com>
2022-08-29 23:51:48 +0200

    Added Leaderboard

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 import './style.css'
+import { uniqueNamesGenerator, NumberDictionary, names } from 'unique-names-generator';
 
 const app = document.getElementById('app')
 
-
 function newGame() {
-  
+
   let flippedNum = 0
   let flippedCards = []
   let moves = 0
-  let timer = 0
-  
-  // DOM
+  let seconds = 0
+  const leaderBoardObj = {}
+
+  // BOARD
   const container = document.createElement('div')
   container.className = 'container'
   app.appendChild(container)
@@ -23,15 +24,23 @@ function newGame() {
   board.className = 'board'
   container.appendChild(board)
 
+  // UI
   const ui = document.createElement('div')
   ui.className = 'ui'
   container.appendChild(ui)
 
+  // SECONDS COUNTER
   const timerDisplay = document.createElement('p')
-  timerDisplay.innerText = `${timer} seconds`
+  timerDisplay.innerText = `${seconds} seconds`
   timerDisplay.style.float = 'left'
   ui.appendChild(timerDisplay)
 
+  let timer = setInterval(() => {
+    seconds += 1;
+    timerDisplay.innerText = `${seconds} seconds`
+  }, 1000)
+
+  // MOVES COUNTER
   const movesNum = document.createElement('p')
   movesNum.innerText = `${moves} moves`
   movesNum.style.float = 'right'
@@ -79,27 +88,56 @@ function newGame() {
         card.element.style.backgroundColor = card.color
 
         // MATCH
-        if (flippedCards.length == 2 && flippedCards[0].color == flippedCards[1].color) {
+        if (flippedCards.length == 2 && flippedCards[0].color == flippedCards[1].color ) {
 
-          let index = cards.map(obj => obj.color).indexOf(flippedCards[0].color)
-          cards.splice(index, 1) // SKETCHY
-          index = cards.map(obj => obj.color).indexOf(flippedCards[0].color)
-          cards.splice(index, 1) // SKETCHY
+          let indexToDelete = cards.map(obj => obj.color).indexOf(flippedCards[0].color)
+          cards.splice(indexToDelete, 1)
 
-          // console.table(cards)
+          indexToDelete = cards.map(obj => obj.color).indexOf(flippedCards[0].color)
+          cards.splice(indexToDelete, 1)
 
           flippedCards = []
           flippedNum = 0
           moves++
 
-          // console.log(moves);
-
           // WIN
-          if (cards.length == 14) {
+          if (cards.length == 0) {
+
+            clearInterval(timer)
 
             board.style.display = 'none'
             h1.innerHTML = 'YOU WIN!'
 
+
+            // 4 LEADERBOARD
+            const inputName = document.createElement('input')
+            inputName.placeholder = 'Insert your name'
+            container.appendChild(inputName)
+
+            // RANDOM NAME GENERATOR
+            const characterName = uniqueNamesGenerator({ dictionaries: [names] });
+            inputName.value = characterName
+
+            const submitName = document.createElement('button')
+            submitName.innerText = 'Sumbit'
+            submitName.className = 'button'
+            container.appendChild(submitName)
+
+            submitName.addEventListener('click', () => {
+
+              leaderBoardObj.name = inputName.value
+              leaderBoardObj.moves = moves
+              leaderBoardObj.seconds = seconds
+
+              container.removeChild(ui)
+              container.removeChild(inputName)
+              container.removeChild(submitName)
+
+              addToLeaderBoard(leaderBoardObj)
+
+            })
+
+            // NEW GANE
             const playButton = document.createElement('button')
             playButton.innerText = 'Play Again'
             playButton.classList.add('button')
@@ -107,10 +145,14 @@ function newGame() {
 
             playButton.addEventListener('click', () => {
               board.innerHTML = ''
-              newGame();
               app.removeChild(playButton)
-              app.removeChild(container)
+              if (document.querySelector('.container')) {
+                app.removeChild(container)
+              }
+              newGame();
             })
+
+
           }
         }
 
@@ -132,22 +174,92 @@ function newGame() {
 
           moves++
 
-          // console.log(moves)
-
         }
 
         movesNum.innerText = `${moves} moves`
+
+
 
       }
     })
   })
 
-  console.table(cards);
-  return cards
+  return leaderBoardObj
+}
+
+function addToLeaderBoard(leaderBoardObj) {
+
+  // DOM
+  document.querySelector('h1').innerText = 'LeaderBoard'
+  const container = document.querySelector('.container')
+
+  const leaderBoardDiv = document.createElement('div')
+  leaderBoardDiv.className = 'leaderBoard'
+  container.appendChild(leaderBoardDiv)
+
+  createLeaderBoardRow(leaderBoardDiv)
+
+  // LEADERBOARD LOGIC
+  let actualLeaderBoard = localStorage.getItem("Leader Board")
+
+  if (actualLeaderBoard == null) {
+
+    actualLeaderBoard = []
+    actualLeaderBoard.push(leaderBoardObj)
+    localStorage.setItem("Leader Board", JSON.stringify(actualLeaderBoard))
+
+  } else {
+
+    actualLeaderBoard = JSON.parse(actualLeaderBoard)
+    actualLeaderBoard.push(leaderBoardObj)
+
+    // SORT LEADERBOARD ARRAY
+    actualLeaderBoard.sort((a, b) => {
+      return (a.moves + a.seconds) - (b.moves + b.seconds)
+    })
+
+    localStorage.setItem("Leader Board", JSON.stringify(actualLeaderBoard))
+  }
+
+  // DOM POPULATE
+  for (let i = 0; i < actualLeaderBoard.length; i++) {
+
+    const leader = actualLeaderBoard[i];
+    const pos = i + 1 + '.'
+    const name = leader.name
+    const moves = leader.moves
+    const secs = leader.seconds
+
+    createLeaderBoardRow(leaderBoardDiv, pos, name, moves, secs,)
+  }
+
+}
+
+function createLeaderBoardRow(
+  leaderBoardEl,
+  pos = '',
+  name = 'name',
+  moves = 'moves',
+  secs = 'seconds') {
+
+  const leaderPosition = document.createElement('div')
+  const leaderName = document.createElement('div')
+  const leaderMoves = document.createElement('div')
+  const leaderSeconds = document.createElement('div')
+
+  leaderPosition.innerText = pos
+  leaderName.innerText = name
+  leaderMoves.innerText = moves
+  leaderSeconds.innerText = secs
+
+  leaderBoardEl.appendChild(leaderPosition)
+  leaderBoardEl.appendChild(leaderName)
+  leaderBoardEl.appendChild(leaderMoves)
+  leaderBoardEl.appendChild(leaderSeconds)
+
 }
 
 newGame()
-
 
 
 
